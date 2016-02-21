212df28863157a2dc1c9c6f8a810f6291173abe6
Scott Crawford <SFC17@pitt.edu>
2016-02-20 21:37:54 -0500

    fix even better!

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
 
 function replace(){
-	document.body.innerHTML = document.body.innerHTML.replace(/shit/ig, "<img src='" + chrome.extension.getURL('src/images/Doge/1.jpg') + "' width="12px" ><img src='" + chrome.extension.getURL('src/images/Doge/2.jpg') + "' width="12px" ><img src='" + chrome.extension.getURL('src/images/Doge/3.jpg') + "'width="12px" ><img src='" + chrome.extension.getURL('src/images/Doge/4.jpg') + "' width="12px">");
+	document.body.innerHTML = document.body.innerHTML.replace(/shit/ig, "<img src='" + chrome.extension.getURL('src/images/Doge/1.jpg') + "' sytle="width:12px" ><img src='" + chrome.extension.getURL('src/images/Doge/2.jpg') + "' sytle="width:12px" ><img src='" + chrome.extension.getURL('src/images/Doge/3.jpg') + "' sytle="width:12px" ><img src='" + chrome.extension.getURL('src/images/Doge/4.jpg') + "' sytle="width:12px">");
 	setTimeout(replace, 500);
 };
 
