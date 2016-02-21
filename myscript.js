ebba156be57c7076a1b9b976315ebc230457710c
Scott Crawford <SFC17@pitt.edu>
2016-02-20 21:32:01 -0500

    fix

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
 
 function replace(){
-	document.body.innerHTML = document.body.innerHTML.replace(/shit/ig, "<img src='" + chrome.extension.getURL('src/images/doge/1.jpg') + "'><img src='" + chrome.extension.getURL('src/images/doge/2.jpg') + "'><img src='" + chrome.extension.getURL('src/images/doge/3.jpg') + "'><img src='" + chrome.extension.getURL('src/images/doge/4.jpg') + "'>");
+	document.body.innerHTML = document.body.innerHTML.replace(/shit/ig, "<img width="12px" src='" + chrome.extension.getURL('src/images/Doge/1.jpg') + "'><img width="12px" src='" + chrome.extension.getURL('src/images/Doge/2.jpg') + "'><img width="12px" src='" + chrome.extension.getURL('src/images/Doge/3.jpg') + "'><img width="12px" src='" + chrome.extension.getURL('src/images/Doge/4.jpg') + "'>");
 	setTimeout(replace, 500);
 };
 
