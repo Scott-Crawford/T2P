c0a9bb84c5258ffac753649118c53577055b69c2
Scott Crawford <SFC17@pitt.edu>
2016-02-20 19:11:09 -0500

    ok

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-window.onload(function(tab) {
+window.onload=function(tab) {
   // No tabs or host permissions needed!
   console.log('changing ' + tab.url + '\'s words!');
   chrome.tabs.executeScript({
@@ -7,4 +7,4 @@ window.onload(function(tab) {
   chrome.tabs.executeScript({
 	code: '$("body").children().html(function (index, text) {this.innerHTML = text.replace(/JavaScript/g, "k")});'
   });
-});
+};
