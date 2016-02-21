2e3b430726e9e6f087c0aad60cd30c688111d48f
Scott Crawford <SFC17@pitt.edu>
2016-02-20 21:53:30 -0500

    fix so it works

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 
 function replace(){
-	document.body.innerHTML = document.body.innerHTML.replace(/shit/ig, "<img src='" + chrome.extension.getURL('src/images/Doge/1.jpg') + "' sytle="width:12px" ><img src='" + chrome.extension.getURL('src/images/Doge/2.jpg') + "' sytle="width:12px" ><img src='" + chrome.extension.getURL('src/images/Doge/3.jpg') + "' sytle="width:12px" ><img src='" + chrome.extension.getURL('src/images/Doge/4.jpg') + "' sytle="width:12px">");
-	setTimeout(replace, 500);
+	document.body.innerHTML = document.body.innerHTML.replace(/shit/ig, "<img src='" + chrome.extension.getURL('src/images/Doge/1.jpg') + "' style=\"width:12px\" ><img src='" + chrome.extension.getURL('src/images/Doge/2.jpg') + "' style=\"width:12px\" ><img src='" + chrome.extension.getURL('src/images/Doge/3.jpg') + "' style=\"width:12px\" ><img src='" + chrome.extension.getURL('src/images/Doge/4.jpg') + "' style=\"width:12px\">");
+	setTimeout(replace, 60000);
 };
 
 replace();
